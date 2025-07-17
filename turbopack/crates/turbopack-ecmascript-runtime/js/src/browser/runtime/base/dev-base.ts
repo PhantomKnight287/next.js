e57f0f5980ce60ae9d01900b8820fad52c4ca423
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-07-17 12:09:32 +0200

    [turbopack] Reuse shared Promise for loaded chunks
    This is still problematic when React sees this special
    Promise for the first time since it'll still need
    a microtask to find out it's already resolved.
    
    The proper fix is to fully instrument the Promise
    in Turbopack (`status`) field so that React can make
    use of the `status` field instead of instrumenting
    it separately.

--- turbopack/crates/turbopack-ecmascript-runtime/js/src/browser/runtime/base/dev-base.ts
+++ turbopack/crates/turbopack-ecmascript-runtime/js/src/browser/runtime/base/dev-base.ts
@@ -606,7 +606,7 @@ function applyChunkListUpdate(update: ChunkListUpdate) {
 
       switch (chunkUpdate.type) {
         case 'added':
-          BACKEND.loadChunk(chunkUrl, { type: SourceType.Update })
+          BACKEND.loadChunkCached(chunkUrl, { type: SourceType.Update })
           break
         case 'total':
           DEV_BACKEND.reloadChunk?.(chunkUrl)
